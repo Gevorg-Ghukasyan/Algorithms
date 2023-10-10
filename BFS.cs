b9aded9d4b530e9f36dcd33a203f64e1fc0c175a
gevorggukasyan24@mail.ru <gevorggukasyan24@mail.ru>
2023-10-10 17:14:04 +0400

    Graph Complete

---
+++ BFS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFSBFS
+{
+    static class BFS
+    {
+        public static HashSet<int> BreadthFirstSearch(int vertex, Dictionary<int, List<int>> graph)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(vertex);
+            while (queue.Count > 0)
+            {
+                int currentVertex = queue.Dequeue();
+                if (!visited.Contains(currentVertex))
+                {
+                    visited.Add(currentVertex);
+                    if (graph.ContainsKey(currentVertex))
+                    {
+                        foreach (int neighbor in graph[currentVertex])
+                        {
+                            if (!visited.Contains(neighbor))
+                            {
+                                visited.Add(neighbor);
+                                queue.Enqueue(neighbor);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return visited;
+
+        }
+
+    }
+}
---
+++ DFS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFSBFS
+{
+    static class DFS
+    {
+
+        #region DFS with HashSET
+        public static void DepthFirstSearchHSet(Dictionary<int, List<int>> graph, int vertex)
+        {
+            HashSet<int> visited = new HashSet<int>();
+            DepthFirstSearchHSet(graph, vertex, visited);
+        }
+
+        public static void DepthFirstSearchHSet(Dictionary<int, List<int>> graph, int vertex, HashSet<int> visited)
+        {
+            visited.Add(vertex);
+            Console.Write($"{vertex} ");
+
+            foreach (int neighbor in graph[vertex])
+            {
+                if (!visited.Contains(neighbor))
+                {
+                    DepthFirstSearchHSet(graph, neighbor, visited);
+                }
+            }
+        }
+        #endregion
+
+
+        public static void DepthFirstSearch(Dictionary<int, List<int>> graph, int vertex, List<bool> visited)
+            {
+                visited[vertex] = true;
+                //        Console.WriteLine("------------------");
+
+                foreach (int neighbor in graph[vertex])
+                {
+                    if (!visited[neighbor])
+                    DepthFirstSearch(graph, neighbor, visited);
+                }
+                Console.Write($"{vertex}, ");
+
+            }
+        
+    }
+
+
+}
